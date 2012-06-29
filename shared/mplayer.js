3d30529ddbf537241e134fcfeb44e2cb7b712cea
Darren Mowat <dmowat91@gmail.com>
2012-06-29 16:30:09 +0100

    Remove some unused routes

--- shared/mplayer.js
+++ shared/mplayer.js
@@ -17,11 +17,13 @@ MPlayer.play = function(song) {
 	if(song == undefined) return;
 	if(mplayer == undefined) {
 		// mplayer = child.spawn('mplayer', ['-slave', song.replace(/(^')|('$)/g, "")]);
+		log.info('Spawning new MPlayer process to play ' + song);
 		mplayer = child.spawn('mplayer', ['-slave', '-quiet', song.replace(/(^')|('$)/g, "")]);
 		paused = false;
 		forcefullyStopped = false;
 		setupEmitters(mplayer);
 	} else {
+		log.info('Telling current MPlayer process to play ' + song);
 		mplayer.stdin.write('loadfile \'' + song + '\'\n');
 		paused = false;
 	}
