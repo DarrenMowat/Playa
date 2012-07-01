3063d02e76c570fef7704ec61cd9e290e25cae0f
Darren Mowat <dmowat91@gmail.com>
2012-07-02 00:45:15 +0100

    MPlayer - Trying to fix stall on loading new file

--- shared/mplayer.js
+++ shared/mplayer.js
@@ -15,10 +15,13 @@ var forcefullyStopped = false;
 
 MPlayer.play = function(song) {
 	if(song == undefined) return;
+	// Tidy the song up
+	song = song.replace(/(^')|('$)/g, "");
+	song = song + "";
 	if(mplayer == undefined) {
 		// mplayer = child.spawn('mplayer', ['-slave', '-quiet', song.replace(/(^')|('$)/g, "")]);
 		log.info('Spawning new MPlayer process to play ' + song);
-		mplayer = child.spawn('mplayer', ['-slave', song.replace(/(^')|('$)/g, "")]);
+		mplayer = child.spawn('mplayer', ['-slave', song]);
 		paused = false;
 		forcefullyStopped = false;
 		setupEmitters(mplayer);
