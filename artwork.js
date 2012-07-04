40c1a765825c48bae4cf74aa6fcda0f0cf93a1fe
Darren Mowat <dmowat91@gmail.com>
2012-07-04 11:47:02 +0100

    Fix bugs with artwork.js

--- artwork.js
+++ artwork.js
@@ -60,7 +60,6 @@ db.getAlbums(function(err, albums) {
       	if(err) {
           throw err;
       	} else if (hasArtwork) { 
-          log.info('Had artwork');
           next();
         } else {
       		// Download artwork
@@ -69,13 +68,11 @@ db.getAlbums(function(err, albums) {
               // Store default images/album.png image
               var url = "/images/album.png";
               db.addAlbumArtwork(url, url, url, album.id, function(err, row) {
-                //if(err) throw err;
-                log.print("Couldn't fetch art for " + album.name + " - " + album.artist_name);
+                if(err) throw err;
               });
             } else {
               db.addAlbumArtwork(images[1]['#text'], images[2]['#text'], images[3]['#text'], album.id, function(err, row) {
-                //if(err) throw err;
-                log.print("Stored art for " + album.name + " - " + album.artist_name);
+                if(err) throw err;
               });
             } 
             next();
--- shared/database.js
+++ shared/database.js
@@ -135,12 +135,9 @@ Database.hasArtwork = function(album_id, callback) {
 	db.serialize(function() {
 		db.get('SELECT * FROM album_view where id = ' + album_id, function(err, row) {
 			if(err) {
-				log.inspect("Error with album " + album_id);
-				log.inspect(err);
-				log.inspect(row);
-				//throw err;
+				callback(err);
 			}
-			return callback(row.albumart_sml != undefined && row.albumart_sml != '');
+			return callback(null, row.albumart_sml != undefined && row.albumart_sml != '');
 		});
 	});
 }
