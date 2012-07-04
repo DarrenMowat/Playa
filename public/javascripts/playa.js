8c6c71c67c9973357c6be1ccb0a2cd9a88070b2f
Darren Mowat <dmowat91@gmail.com>
2012-07-04 20:54:25 +0100

    Fix bug with last commit

--- public/javascripts/playa.js
+++ public/javascripts/playa.js
@@ -33,6 +33,15 @@ function listenNowPlayingUpdates() {
     		items.push('<p>' + data.song.artist_name + ' - ' + data.song.name + '  <a href=\'#\' onclick="return addSongToQueue(\'' + data.song.id + '\')">Requeue</a></p>');
     	}
         $('#nowPlaying').append(items.join(''));
+        //
+        var image = (!data.song.album_art_sml) ? '/images/album.png' : data.song.album_art_sml;
+        $.gritter.add({
+                title: 'Now Playing',
+                text: data.song.artist_name + ' - ' + data.song.name,
+                image: image,
+                sticky: false,
+                time: ''
+            });
     });
 }
 
