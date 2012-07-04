fc0d450fda749932022bc9c5fb868cea0a78df36
Darren Mowat <dmowat91@gmail.com>
2012-07-04 12:19:39 +0100

    Add bootstrap.css spritesheet

--- artwork.js
+++ artwork.js
@@ -63,12 +63,9 @@ db.getAlbums(function(err, albums) {
         } else {
       		// Download artwork
           getAlbumInfo(album.name, album.artist_name, function(err, images) {
-            if(err || images == undefined || images.length == 0) {
-              // Store default images/album.png image
-              var url = "/images/album.png";
-              db.addAlbumArtwork(url, url, url, album.id, function(err, row) {
-                if(err) throw err;
-              });
+            if(err || images == undefined || images.length == 0 || !images[1]['#text']) {
+              // Don't store anything
+              
             } else {
               db.addAlbumArtwork(images[1]['#text'], images[2]['#text'], images[3]['#text'], album.id, function(err, row) {
                 if(err) throw err;
